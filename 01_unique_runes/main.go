dadde5fedf97263c9d2cf463d84725d0012a3212
Jaroslavs Samcuks <yarcat@gmail.com>
2022-01-09 17:36:25 +0100

    01_unique_runes: initial implementation

---
+++ 01_unique_runes/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"unicode"
+)
+
+// UnqiueRunes returns distinct runes and their count.
+func UniqueRunes(s string) map[rune]int {
+	m := make(map[rune]int)
+	for _, r := range s {
+		m[r]++
+	}
+	return m
+}
+
+func main() {
+	fmt.Print("Enter a string: ")
+	s, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+	runeCount := UniqueRunes(s)
+	for r, c := range runeCount {
+		if unicode.IsPrint(r) {
+			fmt.Printf("%U (%c) : %d\n", r, r, c)
+		} else {
+			fmt.Printf("%U (▨) : %d\n", r, c)
+		}
+	}
+}
